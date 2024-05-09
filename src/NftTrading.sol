a5b01be530160c5121dfd4978f3cc87546318fde
IamTalha-Sajid <iamtalha.sajid@gmail.com>
2024-05-09 21:20:03 +0500

    Initialized NFT Trading Project

--- src/NftTrading.sol
+++ src/NftTrading.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED 
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract NftTrading {
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    
+contract NftTrading is Ownable {
+    constructor(address initialOwner) Ownable(initialOwner) {}
 }
